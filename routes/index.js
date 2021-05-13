d2f4cb3e319a2812713172d0047b02fab0e3acef
Vinicius Flores <viniciusflores93@gmail.com>
2021-05-13 19:26:40 -0300

    Sequelize and search route prototype

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const searchController = require('../controllers/search.controller')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/search', searchController.create)
 
 module.exports = router;
